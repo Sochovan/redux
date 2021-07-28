8f6bad5b22f02e2a385dd7a2f6d4245cbd828d5c
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-28 19:20:54 +0300

    test1

---
+++ lesson3/task2/src/index.js
@@ -0,0 +1,8 @@
+import { store, increment, decrement } from "./counter.js";
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(increment());
+store.dispatch(decrement());
