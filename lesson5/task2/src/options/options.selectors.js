953e8ae8010cdb2ff009efc290ffa65309b6aa89
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-07-30 13:11:46 +0300

    test2

--- lesson5/task2/src/options/options.selectors.js
+++ lesson5/task2/src/options/options.selectors.js
@@ -8,19 +8,19 @@ export const selectedIdsSelector = (state) => {
   return state.options.selected;
 };
 
-export const availableOptionsSelector = createSelector(
-  [optionsListSelector, selectedIdsSelector],
-  (allOptionsList, selectedIds) => {
-    return allOptionsList.filter((option) => selectedIds.includes(option.id));
-  }
-);
-
 // export const selectedOptionsSelector = (state) => {
 //   const allOptionslist = optionsListSelector(state);
 //   const selectedIds = selectedIdsSelector(state);
 //   return allOptionslist.filter((option) => selectedIds.includes(option.id));
 // };
 export const selectedOptionsSelector = createSelector(
+  [optionsListSelector, selectedIdsSelector],
+  (allOptionsList, selectedIds) => {
+    return allOptionsList.filter((option) => selectedIds.includes(option.id));
+  }
+);
+
+export const availableOptionsSelector = createSelector(
   [optionsListSelector, selectedIdsSelector],
   (allOptionsList, selectedIds) => {
     return allOptionsList.filter((option) => !selectedIds.includes(option.id));
